5fb0bf619c6aa57387063e9274cfbb6cb6901d58
zemonkamin <95709073+zemonkamin@users.noreply.github.com>
2026-07-11 17:03:27 +0300

    Bug Fix

--- Classes/FL_UILayerDelegate.h
+++ Classes/FL_UILayerDelegate.h
@@ -10,9 +10,17 @@ public:
 	virtual void uiRightPressed() = 0;
 	virtual void uiRightReleased() = 0;
 
-	virtual void uiUpPressed() = 0;   // jump
+	virtual void uiUpPressed() = 0;
 	virtual void uiUpReleased() = 0;
 
-	virtual void uiDownPressed() = 0; // attack
+	virtual void uiDownPressed() = 0;
 	virtual void uiDownReleased() = 0;
+
+	virtual void uiMenuPressed() = 0;
+	virtual void uiPauseRestartPressed() = 0;
+	virtual void uiPauseQuitPressed() = 0;
+	virtual void uiPauseSoundPressed() = 0;
+	virtual bool uiSoundEnabled() const = 0;
+	virtual int uiCurrentHealth() const = 0;
+	virtual int uiMaximumHealth() const = 0;
 };
